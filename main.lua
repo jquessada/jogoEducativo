990df20bbd523645f90cdd285895b66866b140e9
João Quessada <joaoquessada4@gmail.com>
2023-05-01 01:51:50 -0300

    funcionalidades iniciais

---
+++ main.lua
@@ -0,0 +1,127 @@
+local x, y = 280, 280
+local radius = 20
+local speed = 120 -- velocidade da bola em pixels por segundo
+local dx, dy = 0, 0 -- direção da bola
+local money = false
+local socialLife = false
+local fun = false
+local inMoney = false
+local inSocialLife = false
+local inFun = false
+local moneyStatus = 100
+local socialLifeStatus = 100
+local funStatus = 100
+local invest = false --verifica se o usuario quer investir
+local spend = false --verifica se o usuario quer gastar
+local moneyAreaEntered = false -- variável que indica se o círculo entrou na área do dinheiro e se a função investMoney() já foi chamada para essa área
+--variaveis in servem para verificar se o player esta no local
+--variaveis so com o nome servem para verificar se elas precisam ser atualizadas
+--variaveis status armazenam o valor da barra
+
+--essa função verifica a posição do player
+function verifyPosition()
+  -- verifica se a bola está dentro de uma das áreas retangulares
+  if x > 0 and x < 200 and y > 0 and y < 100 then
+    money = true
+    inMoney = true
+    if not moneyAreaEntered then
+      moneyAreaEntered = true
+      investMoney()
+    end
+  elseif x > 600 and x < 800 and y > 0 and y < 100 then
+    socialLife = true
+    inSocialLife = true
+  elseif x > 0 and x < 200 and y > 500 and y < 600 then
+    fun = true
+    inFun = true
+  else 
+    inMoney = false
+    inSocialLife = false
+    inFun = false
+    moneyAreaEntered = false
+  end
+end
+
+function investMoney()
+  if inMoney == true then
+    local message = "Você deseja gastar ou fazer um depósito?"
+    local buttons = {"Gastar", "Depositar"}
+    local pressed = love.window.showMessageBox("Bem vindo ao banco!", message, buttons)
+    if buttons[pressed] == "Depositar" then
+      invest = true
+    else
+      spend = true  
+    end
+  end
+end
+
+
+--essa função verifica se uma das variáveis de status da barra precisa ser atualizada(só quando o player sai da área)
+function updateStatus()
+  if inMoney == false and money == true and invest == true then
+    moneyStatus = moneyStatus + 10
+    money = false
+    invest = false
+  elseif inMoney == false and money == true and spend == true then
+    moneyStatus = moneyStatus - 10
+    money = false
+    spend = false
+  end
+  if inSocialLife == false and socialLife == true then
+    socialLifeStatus = socialLifeStatus - 10
+    socialLife = false
+  end
+  if inFun == false and fun == true then
+    funStatus = funStatus - 10
+    fun = false
+  end
+end
+
+function love.update(dt)
+  verifyPosition()
+  updateStatus()
+  -- atualiza a posição da bola
+  x = x + dx * speed * dt
+  y = y + dy * speed * dt
+  
+  -- verifica se a bola atingiu as bordas da janela
+  if x < radius then
+    x = radius
+  elseif x > love.graphics.getWidth() - radius then
+    x = love.graphics.getWidth() - radius
+  end
+  if y < radius then
+    y = radius
+  elseif y > love.graphics.getHeight() - radius then
+    y = love.graphics.getHeight() - radius
+  end
+end
+
+function love.mousepressed(mx, my, button)
+  -- calcula a direção da bola em relação ao clique do mouse
+  local angle = math.atan2(my - y, mx - x)
+  dx = math.cos(angle)
+  dy = math.sin(angle)
+end
+
+function love.draw()
+  -- barras 
+  love.graphics.setColor(0, 1, 0)
+  love.graphics.rectangle("fill", 300, 0, moneyStatus, 10)
+  love.graphics.setColor(1, 0, 0)
+  love.graphics.rectangle("fill", 300, 20, socialLifeStatus, 10)
+  love.graphics.setColor(0, 0, 1)
+  love.graphics.rectangle("fill", 300, 40, funStatus, 10)
+
+  -- áreas que o usuario pode entrar
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.rectangle("fill", 0, 0, 200, 100)
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.rectangle("fill", 600, 0, 200, 100)
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.rectangle("fill", 0, 500, 200, 100)
+  
+  -- usuário
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.circle("fill", x, y, radius)
+end
